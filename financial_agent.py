bb5458c4bbe97f0a0e4255152324ea00a8f605ce
Mohamed Desoky <mae.desoky@gmail.com>
2025-01-23 15:13:31 -0500

    added changes

--- financial_agent.py
+++ financial_agent.py
@@ -1,10 +1,19 @@
 from phi.agent import Agent
-from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
 import os
+from phi.model.groq import Groq
 
+load_dotenv()
+
+print(f"GROQ_API_KEY: {os.getenv('GROQ_API_KEY')}")
+
+# Verify the key is loaded
 Groq.api_key = os.getenv("GROQ_API_KEY")
+
+if not Groq.api_key:
+    raise ValueError("GROQ_API_KEY not set. Please check your .env file or environment variables.")
 
 # create an Agents
 # websearch Agent
@@ -19,11 +28,14 @@ websearch_agent = Agent(
 )
 
 #financial agent
+# Create a Finance Agent
 finance_agent = Agent(
     name="Finance AI Agent",
-    model =Groq(id="llama-3.3-70b-versatile"),
-    tools= [YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,company_news=True)],
-    instructions=["Use tables to display the data"],
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[
+        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True, company_news=True)
+    ],
+    instructions=["Generate stock price charts and display analyst recommendations."],
     show_tool_calls=True,
     markdown=True,
 )
@@ -37,4 +49,4 @@ multi_ai_agents = Agent(
     markdown=True,
 )
 
-multi_ai_agents.print_response("Summarize analyst recommendation and share the latest news for NVDA",stream=True)
+multi_ai_agents.print_response("Summarize analyst recommendation and share the latest news for TSLA",stream=True)
